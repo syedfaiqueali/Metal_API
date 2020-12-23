061e2abd914f3ca466bd3d51b7d339091124cbf1
Faiq Ali <faiqueali017@gmail.com>
2020-12-23 21:22:41 +0500

    1. Load Tangent and Bitangents

--- 7-Maps & Materials/Project/Materials/Materials/Model.swift
+++ 7-Maps & Materials/Project/Materials/Materials/Model.swift
@@ -32,40 +32,53 @@
 import MetalKit
 
 class Model: Node {
-  
-  let meshes: [Mesh]
-  var tiling: UInt32 = 1
-  let samplerState: MTLSamplerState?
-  
-  init(name: String) {
-    guard
-      let assetUrl = Bundle.main.url(forResource: name, withExtension: nil) else {
-        fatalError("Model: \(name) not found")
+    
+    let meshes: [Mesh]
+    var tiling: UInt32 = 1
+    let samplerState: MTLSamplerState?
+    
+    init(name: String) {
+        guard
+            let assetUrl = Bundle.main.url(forResource: name, withExtension: nil) else {
+            fatalError("Model: \(name) not found")
+        }
+        let allocator = MTKMeshBufferAllocator(device: Renderer.device)
+        let asset = MDLAsset(url: assetUrl,
+                             vertexDescriptor: MDLVertexDescriptor.defaultVertexDescriptor,
+                             bufferAllocator: allocator)
+        //let (mdlMeshes, mtkMeshes) = try! MTKMesh.newMeshes(asset: asset, device: Renderer.device)
+        
+        var mtkMeshes: [MTKMesh] = []
+        let mdlMeshes = asset.childObjects(of: MDLMesh.self) as! [MDLMesh]
+        _ = mdlMeshes.map { mdlMesh in
+            mdlMesh.addTangentBasis(forTextureCoordinateAttributeNamed:
+                                        MDLVertexAttributeTextureCoordinate,
+                                    tangentAttributeNamed: MDLVertexAttributeTangent,
+                                    bitangentAttributeNamed: MDLVertexAttributeBitangent)
+            //Model.vertexDescriptor = mdlMesh.vertexDescriptor
+            mtkMeshes.append(try! MTKMesh(mesh: mdlMesh, device: Renderer.device))
+        }
+        
+        
+        meshes = zip(mdlMeshes, mtkMeshes).map {
+            Mesh(mdlMesh: $0.0, mtkMesh: $0.1)
+        }
+        
+        samplerState = Model.buildSamplerState()
+        super.init()
+        self.name = name
     }
-    let allocator = MTKMeshBufferAllocator(device: Renderer.device)
-    let asset = MDLAsset(url: assetUrl,
-                         vertexDescriptor: MDLVertexDescriptor.defaultVertexDescriptor,
-                         bufferAllocator: allocator)
-    let (mdlMeshes, mtkMeshes) = try! MTKMesh.newMeshes(asset: asset,
-                                                        device: Renderer.device)
-    meshes = zip(mdlMeshes, mtkMeshes).map {
-      Mesh(mdlMesh: $0.0, mtkMesh: $0.1)
+    
+    private static func buildSamplerState() -> MTLSamplerState? {
+        let descriptor = MTLSamplerDescriptor()
+        descriptor.sAddressMode = .repeat
+        descriptor.tAddressMode = .repeat
+        descriptor.mipFilter = .linear
+        descriptor.maxAnisotropy = 8
+        let samplerState =
+            Renderer.device.makeSamplerState(descriptor: descriptor)
+        return samplerState
     }
-    samplerState = Model.buildSamplerState()
-    super.init()
-    self.name = name
-  }
-  
-  private static func buildSamplerState() -> MTLSamplerState? {
-    let descriptor = MTLSamplerDescriptor()
-    descriptor.sAddressMode = .repeat
-    descriptor.tAddressMode = .repeat
-    descriptor.mipFilter = .linear
-    descriptor.maxAnisotropy = 8
-    let samplerState =
-      Renderer.device.makeSamplerState(descriptor: descriptor)
-    return samplerState
-  }
 }
 
 
