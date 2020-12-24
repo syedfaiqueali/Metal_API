5b3bf2a55882b9c7ab9e33548f710e2440a5ab53
Faiq Ali <faiqueali017@gmail.com>
2020-12-24 21:29:55 +0500

    Procedural Animation

--- 8- Character Animation/Project/Animation/Animation/Renderer.swift
+++ 8- Character Animation/Project/Animation/Animation/Renderer.swift
@@ -32,111 +32,123 @@
 import MetalKit
 
 class Renderer: NSObject {
-  static var device: MTLDevice!
-  static var commandQueue: MTLCommandQueue!
-  static var library: MTLLibrary!
-  static var colorPixelFormat: MTLPixelFormat!
-
-  var uniforms = Uniforms()
-  var fragmentUniforms = FragmentUniforms()
-  let depthStencilState: MTLDepthStencilState
-  let lighting = Lighting()
-
-  lazy var camera: Camera = {
-    let camera = ArcballCamera()
-    camera.distance = 3
-    camera.target = [0, 1.3, 0]
-    camera.rotation.x = Float(-15).degreesToRadians
-    return camera
-  }()
-
-  // Array of Models allows for rendering multiple models
-  var models: [Model] = []
-
-  init(metalView: MTKView) {
-    guard
-      let device = MTLCreateSystemDefaultDevice(),
-      let commandQueue = device.makeCommandQueue() else {
-        fatalError("GPU not available")
+    static var device: MTLDevice!
+    static var commandQueue: MTLCommandQueue!
+    static var library: MTLLibrary!
+    static var colorPixelFormat: MTLPixelFormat!
+    
+    var uniforms = Uniforms()
+    var fragmentUniforms = FragmentUniforms()
+    let depthStencilState: MTLDepthStencilState
+    let lighting = Lighting()
+    
+    var currentTime: Float = 0
+    
+    lazy var camera: Camera = {
+        let camera = ArcballCamera()
+        camera.distance = 3
+        camera.target = [0, 1.3, 0]
+        camera.rotation.x = Float(-15).degreesToRadians
+        return camera
+    }()
+    
+    // Array of Models allows for rendering multiple models
+    var models: [Model] = []
+    
+    init(metalView: MTKView) {
+        guard
+            let device = MTLCreateSystemDefaultDevice(),
+            let commandQueue = device.makeCommandQueue() else {
+            fatalError("GPU not available")
+        }
+        Renderer.device = device
+        Renderer.commandQueue = commandQueue
+        Renderer.library = device.makeDefaultLibrary()
+        Renderer.colorPixelFormat = metalView.colorPixelFormat
+        metalView.device = device
+        metalView.depthStencilPixelFormat = .depth32Float
+        
+        depthStencilState = Renderer.buildDepthStencilState()!
+        super.init()
+        metalView.clearColor = MTLClearColor(red: 0.49, green: 0.62,
+                                             blue: 0.75, alpha: 1)
+        metalView.delegate = self
+        mtkView(metalView, drawableSizeWillChange: metalView.bounds.size)
+        
+        // models
+        let ball = Model(name: "beachball.usda")
+        ball.position = [0, 0.35, 0]
+        ball.scale = [100, 100, 100]
+        models.append(ball)
+        let ground = Model(name: "ground.obj")
+        ground.scale = [100, 100, 100]
+        models.append(ground)
+        
+        fragmentUniforms.lightCount = lighting.count
     }
-    Renderer.device = device
-    Renderer.commandQueue = commandQueue
-    Renderer.library = device.makeDefaultLibrary()
-    Renderer.colorPixelFormat = metalView.colorPixelFormat
-    metalView.device = device
-    metalView.depthStencilPixelFormat = .depth32Float
     
-    depthStencilState = Renderer.buildDepthStencilState()!
-    super.init()
-    metalView.clearColor = MTLClearColor(red: 0.49, green: 0.62,
-                                         blue: 0.75, alpha: 1)
-    metalView.delegate = self
-    mtkView(metalView, drawableSizeWillChange: metalView.bounds.size)
-
-    // models
-    let ball = Model(name: "beachball.usda")
-    ball.position = [0, 0.35, 0]
-    ball.scale = [100, 100, 100]
-    models.append(ball)
-    let ground = Model(name: "ground.obj")
-    ground.scale = [100, 100, 100]
-    models.append(ground)
     
-    fragmentUniforms.lightCount = lighting.count
-  }
-  
-
-  static func buildDepthStencilState() -> MTLDepthStencilState? {
-    // 1
-    let descriptor = MTLDepthStencilDescriptor()
-    // 2
-    descriptor.depthCompareFunction = .less
-    // 3
-    descriptor.isDepthWriteEnabled = true
-    return
-      Renderer.device.makeDepthStencilState(descriptor: descriptor)
-  }
+    static func buildDepthStencilState() -> MTLDepthStencilState? {
+        // 1
+        let descriptor = MTLDepthStencilDescriptor()
+        // 2
+        descriptor.depthCompareFunction = .less
+        // 3
+        descriptor.isDepthWriteEnabled = true
+        return
+            Renderer.device.makeDepthStencilState(descriptor: descriptor)
+    }
 }
 
 extension Renderer: MTKViewDelegate {
-  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-    camera.aspect = Float(view.bounds.width)/Float(view.bounds.height)
-  }
-  
-  func draw(in view: MTKView) {
-    guard
-      let descriptor = view.currentRenderPassDescriptor,
-      let commandBuffer = Renderer.commandQueue.makeCommandBuffer(),
-      let renderEncoder =
-      commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
-        return
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        camera.aspect = Float(view.bounds.width)/Float(view.bounds.height)
     }
     
-    renderEncoder.setDepthStencilState(depthStencilState)
-
-    uniforms.projectionMatrix = camera.projectionMatrix
-    uniforms.viewMatrix = camera.viewMatrix
-    fragmentUniforms.cameraPosition = camera.position
+    func draw(in view: MTKView) {
+        guard
+            let descriptor = view.currentRenderPassDescriptor,
+            let commandBuffer = Renderer.commandQueue.makeCommandBuffer(),
+            let renderEncoder =
+                commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
+            return
+        }
+        
+        let deltaTime = 1 / Float(view.preferredFramesPerSecond)
+        update(deltaTime: deltaTime)
+        
+        renderEncoder.setDepthStencilState(depthStencilState)
+        
+        uniforms.projectionMatrix = camera.projectionMatrix
+        uniforms.viewMatrix = camera.viewMatrix
+        fragmentUniforms.cameraPosition = camera.position
+        
+        var lights = lighting.lights
+        renderEncoder.setFragmentBytes(&lights,
+                                       length: MemoryLayout<Light>.stride * lights.count,
+                                       index: Int(BufferIndexLights.rawValue))
+        
+        // render all the models in the array
+        for model in models {
+            renderEncoder.pushDebugGroup(model.name)
+            model.render(renderEncoder: renderEncoder,
+                         uniforms: uniforms,
+                         fragmentUniforms: fragmentUniforms)
+            renderEncoder.popDebugGroup()
+        }
+        
+        renderEncoder.endEncoding()
+        guard let drawable = view.currentDrawable else {
+            return
+        }
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
     
-    var lights = lighting.lights
-    renderEncoder.setFragmentBytes(&lights,
-                                   length: MemoryLayout<Light>.stride * lights.count,
-                                   index: Int(BufferIndexLights.rawValue))
-
-    // render all the models in the array
-    for model in models {
-      renderEncoder.pushDebugGroup(model.name)
-      model.render(renderEncoder: renderEncoder,
-                   uniforms: uniforms,
-                   fragmentUniforms: fragmentUniforms)
-      renderEncoder.popDebugGroup()
+    //To update objects everyframe
+    func update(deltaTime: Float) {
+        currentTime += deltaTime * 4
+        let ball = models[0]
+        ball.position.x = sin(currentTime)
     }
-
-    renderEncoder.endEncoding()
-    guard let drawable = view.currentDrawable else {
-      return
-    }
-    commandBuffer.present(drawable)
-    commandBuffer.commit()
-  }
 }
